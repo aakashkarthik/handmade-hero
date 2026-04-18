bdfdcbb01e2ee6e84654cfe9b12fdb7e4ba66e22
Aakash Karthik <k.aakash@outlook.com>
2026-04-18 21:05:19 +0530

    day 1:creating a simple box and setting up the dev environment

--- game_hero/hero/win32_hero.cpp
+++ game_hero/hero/win32_hero.cpp
@@ -1,9 +1,15 @@
 #include <windows.h>
 
+#pragma warning (disable:28251)
+
 int WinMain(HINSTANCE instance,
-			HINSTANCE previnstance,
+			HINSTANCE,
 			LPSTR cmdline, 
 			int cmdshow)
 {
+	UNREFERENCED_PARAMETER(instance);
+	UNREFERENCED_PARAMETER(cmdline);
+	UNREFERENCED_PARAMETER(cmdshow);
+	MessageBox(0, TEXT("This is NOT a message"), TEXT("heading"), MB_ABORTRETRYIGNORE | MB_ICONERROR);
 	return 0;
 }
