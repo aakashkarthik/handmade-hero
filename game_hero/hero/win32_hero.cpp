e3c59a79d0ce79f37a8c5928566965158582a455
Aakash Karthik <k.aakash@outlook.com>
2026-04-30 21:06:14 +0530

    removed message box and wrote a windowsproc function

--- game_hero/hero/win32_hero.cpp
+++ game_hero/hero/win32_hero.cpp
@@ -2,14 +2,45 @@
 
 #pragma warning (disable:28251)
 
+LRESULT CALLBACK Manage(
+	HWND Window,
+	UINT Message,
+	WPARAM Param1,
+	LPARAM Param2
+)
+{
+	LRESULT result = 0;
+	PAINTSTRUCT paint;
+	switch (Message)
+	{
+	case WM_PAINT:
+	{
+		BeginPaint(Window, &paint);
+		EndPaint(Window, &paint);
+	}break;
+	case WM_DESTROY:
+	{
+		PostQuitMessage(0);
+	}break;
+	default:
+		result = DefWindowProc(Window, Message, Param1, Param2);
+	}
+	return result;
+}
+
 int WinMain(HINSTANCE instance,
 			HINSTANCE,
 			LPSTR cmdline, 
 			int cmdshow)
 {
-	UNREFERENCED_PARAMETER(instance);
 	UNREFERENCED_PARAMETER(cmdline);
 	UNREFERENCED_PARAMETER(cmdshow);
-	MessageBox(0, TEXT("This is NOT a message"), TEXT("heading"), MB_ABORTRETRYIGNORE | MB_ICONERROR);
+	WNDCLASS WindowClass = {};
+	WindowClass.style = CS_HREDRAW|CS_VREDRAW|CS_OWNDC;
+	WindowClass.lpfnWndProc = Manage;
+	WindowClass.hInstance = instance;
+	WindowClass.lpszClassName = TEXT("handmadeherowindow");
+
+	RegisterClass(&WindowClass);
 	return 0;
 }
